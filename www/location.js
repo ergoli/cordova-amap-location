067813c0ee6a1ec154e5542599878022c0459b0f
XinJian Sun <997769550@qq.com>
2016-08-25 10:20:18 +0800

    android 版本 getCurrentPosition测试完成

--- www/location.js
+++ www/location.js
@@ -1,17 +1,18 @@
 var exec = cordova.require('cordova/exec');
-
-
 module.exports = {
-    getCurrentPosition: function(success, error, args) {     
+    getCurrentPosition: function(success, error, args) { 
+    	console.log('getCurrentPosition');    
         exec(success, error, "Location", "getCurrentPosition", []);
     },
 
     watchPosition: function(success, error, args) {
-        
+        console.log('watchPosition');    
+        exec(success, error, "Location", "watchPosition", []);
     },
 
     clearWatch: function(pluginWatchId) {
-      
+       console.log('clearWatch'); 
+       exec(success, error, "Location", "clearWatch", [pluginWatchId]);
     }
 };
 
